94712d6800c23c2567bedba9b0da9dac983fa7a7
Yunjeong Choi <yoonjeong04@sookmyung.ac.kr>
2022-08-21 19:40:07 +0900

    Feat: 네이버 소셜 로그인

--- src/main/java/com/ahreumvitsnowflake/graduation/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/ahreumvitsnowflake/graduation/springboot/config/auth/dto/OAuthAttributes.java
@@ -13,7 +13,6 @@ public class OAuthAttributes {
     private String nameAttributeKey;
     private String username;
     private String email;
-    // private String emailVerifiedYn;
     private String picture;
 
     @Builder
@@ -24,17 +23,34 @@ public class OAuthAttributes {
         this.email = email;
         this.picture = picture;
     }
-
+    
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
+    // 구글 생성자
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes){
         return OAuthAttributes.builder()
                 .username((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    // 네이버 생성자
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .username((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
