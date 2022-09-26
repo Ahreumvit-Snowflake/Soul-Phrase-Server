4d20ad7c15e2fff9964170919eb1358d5ff30eb8
Yunjeong Choi <yoonjeong04@sookmyung.ac.kr>
2022-09-27 02:43:57 +0900

    Feat: 스프링 CORS 설정

--- src/main/java/com/ahreumvitsnowflake/graduation/springboot/config/WebConfig.java
+++ src/main/java/com/ahreumvitsnowflake/graduation/springboot/config/WebConfig.java
@@ -3,7 +3,9 @@ package com.ahreumvitsnowflake.graduation.springboot.config;
 import com.ahreumvitsnowflake.graduation.springboot.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +18,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
         argumentResolvers.add(loginUserArgumentResolver);
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                );
     }
 }
