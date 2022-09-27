1d2a58ad890ca50941b0a0a3aac024e970fdf132
Yunjeong Choi <yoonjeong04@sookmyung.ac.kr>
2022-09-28 04:17:49 +0900

    Fix: config 변경

--- src/main/java/com/ahreumvitsnowflake/graduation/springboot/config/WebConfig.java
+++ src/main/java/com/ahreumvitsnowflake/graduation/springboot/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://ec2-54-180-180-137.ap-northeast-2.compute.amazonaws.com:3000")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
